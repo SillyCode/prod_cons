06e5310d5fef7967003f0ab33886d89db729f9cd
Mark Malin <mark.malin@hexagon.com>
2021-03-31 15:28:58 +0300

    initial commit

---
+++ consumer.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+const FILEPATH = "./data/messages.txt"
+
+func read_from_file(offset int64) int64 {
+	f, e := os.Open(FILEPATH)
+	if e != nil {
+		panic(e)
+	}
+
+	defer f.Close()
+
+	_, err := f.Seek(offset, 0)
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(f)
+	var line string
+	for {
+		line, err = reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			break
+		}
+		fmt.Printf("%s", line)
+
+		if err != nil {
+			break
+		}
+	}
+	if err != io.EOF {
+		panic(err)
+	}
+	new_pos, err := f.Seek(0, 2)
+	if err != nil {
+		panic(err)
+	}
+	return new_pos
+}
+
+func main() {
+	c := time.Tick(3 * time.Second)
+	pos := int64(0)
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	for _ = range c {
+		pos = read_from_file(pos)
+	}
+}
---
+++ producer.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const FILEPATH = "./data/messages.txt"
+
+func write_to_file(message string) {
+	file_handler, err := os.OpenFile(FILEPATH, os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file_handler.Close()
+
+	writer := bufio.NewWriter(file_handler)
+	_, err = writer.WriteString(fmt.Sprintf("%s", message))
+	if err != nil {
+		panic(err)
+	}
+	writer.Flush()
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Write any message. To exit type CTRL+C")
+	for {
+		body, _ := reader.ReadString('\n')
+		write_to_file(body)
+		log.Printf(" [x] Wrote message: %s", body)
+	}
+}
